9fb6d190789a1d946db87a892e5bf9793c303667
= <=>
2020-09-14 14:56:27 +0700

    Commit 14/09/20

---
+++ src/main/java/com/api/example_api/Example/JSON_Example.java
@@ -0,0 +1,72 @@
+package com.api.example_api.Example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.api.example_api.model.Data;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class JSON_Example {
+	public static Data getData(String url) throws ClientProtocolException, IOException {
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(url);
+		httpGet.addHeader("accept","application/json");
+		HttpResponse response = httpClient.execute(httpGet);
+		String data = readData(response);
+		Gson gson = new Gson();
+		return gson.fromJson(data, Data.class);
+	}
+	
+	public static List<Data> getDataList(String url) throws ClientProtocolException, IOException {
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(url);
+		httpGet.addHeader("accept","application/json");
+		HttpResponse response = httpClient.execute(httpGet);
+		String data = readData(response);
+		Gson gson = new Gson();
+		Type type = new TypeToken<List<Data>>(){}.getType();
+		return gson.fromJson(data, type);
+	}
+	
+	public static String readData(HttpResponse httpResponse) throws IOException {
+		BufferedReader bufferedReader = null;
+		try {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
+			StringBuffer buffer = new StringBuffer();
+			char[] dataLength = new char[1024];
+			int read;
+			while((read = bufferedReader.read(dataLength)) != -1) {
+				buffer.append(dataLength, 0, read);
+			}
+			return buffer.toString();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(bufferedReader != null) {
+				bufferedReader.close();
+			}
+		}
+		return null;
+	}
+	public static void main(String[] args) {
+		try {
+			Data data = getData("https://reqres.in/api/users?page=2");
+			System.out.println(data);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/com/api/example_api/model/Data.java
@@ -0,0 +1,72 @@
+package com.api.example_api.model;
+
+import java.util.List;
+
+public class Data{
+	private int page;
+	private int per_page;
+	private int total;
+	private int total_page;
+	private List<User> data;
+	private Ad ad;
+	
+    public Ad getAd() {
+		return ad;
+	}
+
+	public void setAd(Ad ad) {
+		this.ad = ad;
+	}
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+	public int getPer_page() {
+		return per_page;
+	}
+
+	public void setPer_page(int per_page) {
+		this.per_page = per_page;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public int getTotal_page() {
+		return total_page;
+	}
+
+	public void setTotal_page(int total_page) {
+		this.total_page = total_page;
+	}
+
+	public List<User> getData() {
+		return data;
+	}
+
+	public void setData(List<User> data) {
+		this.data = data;
+	}
+
+	@Override
+    public String toString()
+    {
+        return	"page: " + page + "\n" +
+        		"per_ page: " + per_page + "\n" +
+        		"total: " + total + "\n" +
+        		"total_page: " + total_page + "\n" +
+        		"data: " + data + "\n" +
+        		"ad: " + ad;
+    }
+}
+
